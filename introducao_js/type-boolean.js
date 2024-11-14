686c832f48d3aba957eba474d173c497f2c825b0
Jakson Fritzen <jaksoneufrasio@gmail.com>
2024-11-13 21:47:52 -0300

    Javascript 13-11-2024

---
+++ introducao_js/type-boolean.js
@@ -0,0 +1,27 @@
+const value1 = true;
+const value2 = false;
+
+console.log(value1 == value2)
+
+//Thrusty / Falsy -> Variáveis vazias ou com valor 0, se submeter a um teste lógico, se tornarão um tipo boolea (thrusty/falsy)
+
+
+//Teste lógico 1
+if (value1) {
+    console.log('verdadeiro')
+} else {
+    console.log('falso')
+}
+
+//Teste lógico 1
+if (value1) {
+    console.log('verdadeiro')
+}
+
+//O de cima vira:
+
+if (value1) console.log('verdadeiro');
+
+//Operador ternário
+
+const resultado = value1 ? 'verdadeiro' : 'falso';
